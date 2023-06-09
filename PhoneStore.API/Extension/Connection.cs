ebe74e34e0009cf79987001c545cfae1a642717a
victor Gilbert <victorgilbert63@gmail.com>
2023-06-09 13:08:20 +0100

    Added connection to database

---
+++ PhoneStore.API/Extension/Connection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneStore.DAL.Context;
+
+namespace PhoneStore.API.Extension
+{
+    public static class Connection
+    {
+        public static void AddConnection(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            services.AddDbContext<AppDbContext>(dpOption =>
+            {
+                var ConnectionString = builder.Configuration.GetSection("ConnectionStrings")["ConnString"];
+                dpOption.UseSqlServer(ConnectionString);
+            });
+        }
+    }
+}
+ 
--- PhoneStore.API/Program.cs
+++ PhoneStore.API/Program.cs
@@ -1,14 +1,16 @@
 using PhoneStore.DAL.Context;
 using Microsoft.EntityFrameworkCore;
+using PhoneStore.API.Extension;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectiion")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddConnection(builder);
+
 
 builder.Services.AddCors(options =>
 {
